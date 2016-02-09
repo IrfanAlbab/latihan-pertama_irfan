b88f564ba923bc9f74a5f5a1dc010e3abdae0a9b
irfanalbab <muhammadirfanalbab@gmail.com>
2016-02-09 23:03:56 +0700

    update

---
+++ bintang.php
@@ -0,0 +1,10 @@
+<?php
+	for($a=1;$a<=4;$a++){
+		for($b=$a;$b>=1;$b--) {
+			echo "*";}
+	echo "<br>";}
+	for($c=1;$c<=3;$c++){
+		for($d=3;$d>=$c;$d--){
+			echo "*";}
+	echo "<br>";}
+?>
---
+++ entri.php
@@ -0,0 +1,28 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+	$arrFile = array("ADL.txt", "AG.txt", "GA.txt");
+	
+	function bacaFile($namafile) {
+		
+		global $dir;
+		
+		$file = fopen(getcwd()."/".$namafile, "r");
+		$baris = fgets($file);
+		
+		while(($baris = fgets($file)) ==! FALSE) {
+			
+			$data = explode(",", $baris);
+			
+			if (count($data) > 3) {
+				$strAngkot = $data[1];
+				echo $strAngkot."<br />";
+			}
+			
+		}
+	}
+	
+	for ($i=0; $i<count($arrFile); $i++) {
+		bacaFile($arrFile[$i]);
+	}
+?>
